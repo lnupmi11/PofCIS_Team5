a2ead340d5ca94946a050086e2fbc25fadbb31fd
Ira Dmytriv <iruna.dmutriv99@gmail.com>
2018-10-15 18:24:03 +0300

    Unit tests for Hexagon

---
+++ HexagonVisualisator/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HexagonVisualisatorDAL.Models;
+using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class HexagonTests
+    {
+        [TestMethod]
+        public void Test_PointsProperty()
+        {
+            Hexagon Hexagon = new Hexagon();
+            List<Point> points = new List<Point>
+            {
+                new Point(67, 89),
+                new Point(67, 45),
+                new Point(56, 23),
+                new Point(10, 9),
+                new Point(100, 145),
+                new Point(23, 76)
+            };
+            Hexagon.Vertexes = points;
+            Assert.AreEqual(Hexagon.Vertexes, points);
+        }
+
+        [TestMethod]
+        public void Test_HexagonColorProperty()
+        {
+            Hexagon Hexagon = new Hexagon();
+            Hexagon.Color = Colors.Red;
+            Assert.AreEqual(Hexagon.Color, Colors.Red);
+        }
+    }
+}
