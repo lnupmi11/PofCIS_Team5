c92c8591535f0d3f71c97eeb6f14a69ba3430a82
Ira Dmytriv <polina.dalevskaya@gmail.com>
2018-11-19 20:50:29 +0200

    Added tests for DAL

---
+++ RealEstateBrokerage/RealEstateTestsProject/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RealEstateBrokerage.DAL.IOTypes;
+
+namespace RealEstateTestsProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CityDbTest()
+        {
+            CitiesDB cities = new CitiesDB("CitiesData.txt");
+            cities.ReadFromFile();
+            Assert.AreEqual(cities.AllCities.Count, 8);
+        }
+
+        [TestMethod]
+        public void DistrictsDbTest()
+        {
+            DistrictsDB districts = new DistrictsDB("DistrictsData.txt");
+            districts.ReadFromFile();
+            Assert.AreEqual(districts.AllDistricts.Count, 14);
+        }
+
+        [TestMethod]
+        public void RealEstateDbTest()
+        {
+           RealEstateDB realEstate = new RealEstateDB("RealEstateData.txt");
+           realEstate.ReadFromFile();
+           Assert.AreEqual(realEstate.AllRealEstate.Count, 14);
+        }
+    }
+}
