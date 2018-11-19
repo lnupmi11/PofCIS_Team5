2c09dc53dfb268550b4f249319313dcd4dff38b4
Ira Dmytriv <iruna.dmutriv99@gmail.com>
2018-11-19 20:45:37 +0200

    Added Test poject  && BLL tests

---
+++ RealEstateBrokerage/RealEstateTestsProject/BLLTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RealEstateBrokerage.BLL;
+using RealEstateBrokerage.DAL.IOTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateTestsProject
+{
+    [TestClass]
+    public class BLLTests
+    {
+        [TestMethod]
+        public void DistricByIdTest()
+        {
+            RealEstateBrokerageManager rebm = new RealEstateBrokerageManager();
+            string district = rebm.GetDistrictById(1).Name;
+            Assert.AreEqual(district, "Soho");
+        }
+
+        [TestMethod]
+        public void DistricByNameTest()
+        {
+            RealEstateBrokerageManager rebm = new RealEstateBrokerageManager();
+            int districtId = rebm.GetDistrictByName("Soho").Id;
+            Assert.AreEqual(districtId, 1);
+        }
+
+        [TestMethod]
+        public void DistricByCityIdTest()
+        {
+            RealEstateBrokerageManager rebm = new RealEstateBrokerageManager();
+            string district = rebm.GetDistrictsByCityId(1).FirstOrDefault().Name;
+            Assert.AreEqual(district, "Soho");
+        }
+
+        [TestMethod]
+        public void CityByIdTest()
+        {
+            RealEstateBrokerageManager rebm = new RealEstateBrokerageManager();
+            string city = rebm.GetCityById(1).Name;
+            Assert.AreEqual(city, "NewYork");
+        }
+
+        [TestMethod]
+        public void CityByNameTest()
+        {
+            RealEstateBrokerageManager rebm = new RealEstateBrokerageManager();
+            int cityId = rebm.GetCityByName("NewYork").Id;
+            Assert.AreEqual(cityId, 1);
+        }
+
+        [TestMethod]
+        public void RealEstateByCityIdTest()
+        {
+            RealEstateBrokerageManager rebm = new RealEstateBrokerageManager();
+            double price = rebm.GetRealEstateById(1).Price;
+            Assert.AreEqual(price, 350);
+        }
+
+        [TestMethod]
+        public void RealEstateByDistrictId()
+        {
+            RealEstateBrokerageManager rebm = new RealEstateBrokerageManager();
+            double price = rebm.GetRealEstateByDistrictId(1).FirstOrDefault().Price;
+            Assert.AreEqual(price, 350);
+        }
+    }
+}
---
+++ RealEstateBrokerage/RealEstateTestsProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RealEstateTestsProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RealEstateTestsProject")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee890f8c-e084-4e09-b837-0ed0b4e881d1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
