5fa4668e2077426cbb5553d0fcb43da9a827d17d
Ira Dmytriv <polina.dalevskaya@gmail.com>
2018-11-26 22:10:43 +0200

    -- Added testt for Task implemetation cases

---
+++ ADO.NET/ADOTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ADOTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ADOTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1df60154-9a4c-4449-b5ef-f0d68d74a6b3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ADO.NET/ADOTests/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using ADO.NET;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ADOTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShowInformationAboutEmployeeWithId()
+        {
+            TaskImplementation implementation = new TaskImplementation();
+            implementation.OpenConnection();
+            string result = implementation.ShowInformationAboutEmployeeWithId(8);
+            implementation.CloseConnection();
+            result = result.Substring(0, result.IndexOf('\n'));
+            Assert.IsTrue(result == "LastName: Callahan ");
+        }
+
+        [TestMethod]
+        public void ShowListOfEmployeeFirstAndLastNamesWhichStartsOn()
+        {
+            TaskImplementation implementation = new TaskImplementation();
+            implementation.OpenConnection();
+            string result = implementation.ShowListOfEmployeeFirstAndLastNamesWhichStartsOn('A');
+            implementation.CloseConnection();
+            result = result.Substring(0, result.IndexOf('\n'));
+            Assert.IsTrue(result == "Andrew - Fuller");
+        }
+
+        [TestMethod]
+        public void ShowListOfEmployeeFirstAndLastNamesFromCity()
+        {
+            TaskImplementation implementation = new TaskImplementation();
+            implementation.OpenConnection();
+            string result = implementation.ShowListOfEmployeeFirstAndLastNamesFromCity("London");
+            implementation.CloseConnection();
+            result = result.Substring(0, result.IndexOf('\n'));
+            Assert.IsTrue(result == "Steven - Buchanan");
+        }
+
+        [TestMethod]
+        public void ShowListOfEmployeeFirstAndLastNamesWithAgeMoreThan()
+        {
+            TaskImplementation implementation = new TaskImplementation();
+            implementation.OpenConnection();
+            string result = implementation.ShowListOfEmployeeFirstAndLastNamesWithAgeMoreThan(55);
+            implementation.CloseConnection();
+            result = result.Substring(0, result.IndexOf('\n'));
+            Assert.IsTrue(result == "Steven - Buchanan - 63");
+        }
+
+        [TestMethod]
+        public void ShowCountEmployeesOfCity()
+        {
+            TaskImplementation implementation = new TaskImplementation();
+            implementation.OpenConnection();
+            string result = implementation.ShowCountEmployeesOfCity("London");
+            implementation.CloseConnection();
+            result = result.Substring(0, result.IndexOf('\n'));
+            Assert.IsTrue(result == "London - 4");
+        }
+    }
+}
